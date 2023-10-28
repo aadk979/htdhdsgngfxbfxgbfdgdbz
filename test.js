3c1a517503093208e4373ca6f74bed118649066b
aadk979 <138833125+aadk979@users.noreply.github.com>
2023-10-28 21:51:32 +0800

    Update test.js

--- test.js
+++ test.js
@@ -12,27 +12,12 @@ const io = new Server(server, {
         methods: ["GET", "POST"]
     }
 });
-// Assuming you have a Socket.IO server instance named 'io'
-function disconnectAllUsers() {
-    // Get a list of connected sockets
-    const connectedSockets = io.sockets.sockets;
-
-    // Loop through each socket and disconnect it
-    for (const socketId in connectedSockets) {
-        const socket = connectedSockets[socketId];
-        socket.disconnect(true); // Close the socket
-    }
-}
-
-
 
 io.on("connection", (socket) => {
-    const userIP = socket.handshake.address;
-    console.log(userIP);
     console.log('A user connected');
     
     socket.on("redirect-req", n =>{
-        io.emit("redirect" , ("http://google.com"));
+        io.emit("redirect" , ("https://theaffanchatlivechat.000webhostapp.com"));
     });
 
     let x = true;
@@ -62,10 +47,12 @@ io.on("connection", (socket) => {
         if (authkey === "iloveamelie" && authcode === "260908180608") { 
             io.emit("server", "Server has shut down");
             disconnectAllUsers();
-            server.close();
+            server.close(()=>{
+                console.log("!!server terminated!!")
+            });
             io.emit("server", "Server has shut down");
             
-            console.log("shutdown success")
+            
             
         } else {
             io.emit("server", "Authentication failed, unable to shut down the server.");
