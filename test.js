6c1e470b74e48573967b806cea148648a147ac14
aadk979 <138833125+aadk979@users.noreply.github.com>
2023-11-03 20:33:52 +0800

    Update test.js

--- test.js
+++ test.js
@@ -15,9 +15,39 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-    setInterval(()=>{
-    	io.emit("message", ({sender: "User 1". , message: "did yo get the message nigger"}) )    
-    },1000);
+    console.log('A user connected');
+
+    socket.on("redirect-req", (n) => {
+        io.emit("redirect", "https://theaffanchatlivechat.000webhostapp.com");
+    });
+
+    let x = true;
+    socket.on('chat message', (message) => {
+        while (x === true) {
+            io.emit('chat message', message);
+            console.log('messages sent');
+            x = false;
+        };
+        setInterval(() => {
+            x = true;
+        }, 100);
+    });
+
+    socket.on("onuser", (t) => {
+        io.emit("usern", t);
+        username = t;
+    });
+
+    socket.on("out", (w) => {
+        io.emit("out", w);
+        console.log(w + " logged out");
+    });
+
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        io.emit("out", username);
+    });
 });
 
 const PORT = process.env.PORT || 5500;
